a61c2fd420d05c1519c6ca1a404aa79272df7c14
Julia <juliafarganus@gmail.com>
2024-04-30 21:33:39 +0200

    continue with report

--- ai_data_eng/halma_game/evaluation/reading.py
+++ ai_data_eng/halma_game/evaluation/reading.py
@@ -6,13 +6,19 @@ import pandas as pd
 from ai_data_eng.halma_game.evaluation.computation import jump_size
 
 
+def match_name(match_dir):
+    return '_'.join(str(match_dir).split('\\')[-3:])
+
+
 def get_configuration(match_dir, date_prefix):
     elements = str(match_dir).split('\\')
     strategies, depths, algorithms = elements[-1].split('-'), elements[-2].split('-'), elements[-3].split('-')
+    if len(algorithms) > 2:
+        algorithms = ['-'.join(algorithms[:2]), '-'.join(algorithms[2:])]
     if os.path.exists(match_dir / f'{date_prefix}-stats'):
         match_results = pd.read_csv(match_dir / f'{date_prefix}-stats', sep=';', names=['Total moves', 'Winner'])
     else:
-        match_results = pd.DataFrame(data={'Total moves': [len(player_white) + len(player_black)], 'Winner': ['None']})
+        match_results = pd.DataFrame(data={'Total moves': [0], 'Winner': ['None']})
     config = {
         'black': {
             'strategy': strategies[0].split('.')[1].lower(), 'depth': depths[0], 'algorithm': algorithms[0]
@@ -20,7 +26,7 @@ def get_configuration(match_dir, date_prefix):
         'white': {
             'strategy': strategies[1].split('.')[1].lower(), 'depth': depths[1], 'algorithm': algorithms[1]
         },
-        'match': ''.join(elements[-3:]),
+        'match': match_name(match_dir),
         'winner': match_results.loc[0, 'Winner']
     }
 
@@ -41,7 +47,7 @@ def add_metadata(dfs, players, match_dir, date_prefix):
 def read_match(match_dir: Path, date_prefix: str=None):
     header = ['Move from', 'Move to', 'Compute time', 'Tree size', 'Eval value']
     if not date_prefix:
-        date_prefix = '-'.join(os.listdir(match_dir)[0].split('-')[:2])
+        date_prefix = '-'.join(str(os.listdir(match_dir)[0]).split('-')[:2])
     player_black = pd.read_csv(match_dir / f'{date_prefix}-PLAYER.BLACK',
                                        names=header, sep=';')
     player_white = pd.read_csv(match_dir / f'{date_prefix}-PLAYER.WHITE',
